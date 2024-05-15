8fe5f35fc5ac98ffd9c241c287a2d93a5e2a2867
jolly <jolly@88tech.net>
2024-05-15 18:01:58 +0800

    part of code

--- js/page/RegistPage.tsx
+++ js/page/RegistPage.tsx
@@ -1,4 +1,17 @@
-import React, { useState } from "react";
-export default (props: any) => { 
+import React, { Component, useState } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+export default class Index extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>最热</Text>
+            </View>
+        );
+    }
+}
 
-}; 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
--- js/page/navigator/AppNavigator.tsx
+++ js/page/navigator/AppNavigator.tsx
@@ -13,17 +13,83 @@ import PopularPage from "../PopularPage";
 import SearchPage from "../SearchPage";
 import SortKeyPage from "../SortKeyPage";
 import WebviewPage from "../WebviewPage";
+import WelcomPage from "../WelcomPage";
 import AboutMePage from "../about/AboutMePage";
 import AboutPage from "../about/AboutPage";
 
-export default class Index extends Component {
-    render() {
+const Stack = createNativeStackNavigator();
+export default function App(){
         return (
-            <View style={styles.container}>
-                <Text>最热</Text>
-            </View>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="LoginPage"
+                        component={LoginPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="RegistPage"
+                        component={RegistPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="HomePage"
+                        component={HomePage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="CodePushPage"
+                        component={CodePushPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="CustomKeyPage"
+                        component={CustomKeyPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="DetailPage"
+                        component={DetailPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="PopularPage"
+                        component={PopularPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="SearchPage"
+                        component={SearchPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="SortKeyPage"
+                        component={SortKeyPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="WebviewPage"
+                        component={WebviewPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="WelcomPage"
+                        component={WelcomPage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="AboutMePage"
+                        component={AboutMePage}
+                        options={{ headerShown:false }}
+                    />
+                    <Stack.Screen
+                        name="AboutPage"
+                        component={AboutPage}
+                        options={{ headerShown:false }}
+                    />
+                </Stack.Navigator>
+           </NavigationContainer>
         );
-    }
 }
 
 const styles = StyleSheet.create({
